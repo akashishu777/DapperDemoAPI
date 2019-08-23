97a93fb7d28eddf7155cf68c5178c00c436b30b2
Akash Vishwakarma <14cse031giet@gmail.com>
2019-08-23 17:39:24 +0530

    first commit

---
+++ DapperDemoAPI/DAL/ICustomerRespository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DapperDemoAPI.Models;
+
+namespace DapperDemoAPI.DAL
+{
+	internal interface ICustomerRespository
+	{
+		List<Customer> GetCustomers();
+
+		Customer GetSingleCustomer(int customerId);
+
+		bool InsertCustomer(Customer ourCustomer);
+
+		bool DeleteCustomer(int customerId);
+
+		bool UpdateCustomer(Customer ourCustomer);
+	}
+}
---
+++ SQL/customer.sql
@@ -0,0 +1,35 @@
+﻿USE [DapperDemo]
+GO
+
+/****** Object:  Table [Customer] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Customer](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerFirstName] [varchar](50) NULL,
+	[CustomerLastName] [varchar](50) NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+  insert into [DapperDemo].[dbo].[Customer] values ('Shubham', 'Sharma', 0)
+    insert into [DapperDemo].[dbo].[Customer] values ('Anas', 'Khan', 1)
+	  insert into [DapperDemo].[dbo].[Customer] values ('Suresh', 'Gorai', 1)
+	    insert into [DapperDemo].[dbo].[Customer] values ('Vivek', 'Sinha', 0)
+		  insert into [DapperDemo].[dbo].[Customer] values ('Prajna', 'jyoti', 1)
+
+SET ANSI_PADDING OFF
+GO
+
